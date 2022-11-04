4d8cf53a160d834b561b3097595a8ee2865668cd
githubotoro <yupuday.git@gmail.com>
2022-11-04 21:46:13 +0530

    google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,33 @@
 import "../styles/globals.css";
 import "../styles/gradients.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.GOOGLE_ANALYTICS}`}
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-${process.env.GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+				}}
+			/>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
 
 export default MyApp;
