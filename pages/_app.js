8ba7a0a5d24e27dc22db483deb08208696247016
githubotoro <yupuday.git@gmail.com>
2022-11-05 13:38:39 +0530

    google-analytics-addition

--- pages/_app.js
+++ pages/_app.js
@@ -10,23 +10,19 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Script
-				strategy="afterInteractive"
-				src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.GOOGLE_ANALYTICS}`}
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
 			/>
-			<Script
-				id="google-analytics"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-${process.env.GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-        `,
-				}}
-			/>
+			<Script strategy="lazyOnload">
+				{`
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+		page_path: window.location.pathname,
+		});
+	`}
+			</Script>
 			<Component {...pageProps} />\
 			<Analytics />
 		</>
